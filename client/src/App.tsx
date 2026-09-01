9d1047588ce4021aa3f5631de977fb1cb73521b6
Developer <dev@example.com>
2026-09-01 15:01:18 +0900

    feat: set dark mode as the default theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider
-        defaultTheme="light"
+        defaultTheme="dark"
         switchable
       >
         <TooltipProvider>
